a373a68adc93e77cb3a2286dc89b5a88e0275f60
nirvanasan <alexander.correa@utp.edu.co>
2024-12-16 11:28:16 -0500

    .

--- Backend/resources/js/components/navegacionView.vue
+++ Backend/resources/js/components/navegacionView.vue
@@ -1,58 +1,97 @@
-
 <template>
   <nav class="navigation">
     <ul class="main-nav">
       <div class="main-nav__item">
-        <router-link to="/eleccion" class="create-account nav-link">Dofa</router-link>
+        <router-link 
+          to="/eleccion" 
+          class="create-account nav-link" 
+          :class="{ 'selected': selectedButton === '/eleccion' }"
+          @click="selectedButton = '/eleccion'"
+        >
+          Dofa
+        </router-link>
       </div>
       <div class="main-nav__item">
-        <router-link to="/clasificacion" class="create-account nav-link">Clasificación</router-link>
+        <router-link 
+          to="/clasificacion" 
+          class="create-account nav-link" 
+          :class="{ 'selected': selectedButton === '/clasificacion' }"
+          @click="selectedButton = '/clasificacion'"
+        >
+          Clasificación
+        </router-link>
       </div>
       <div class="main-nav__item">
-        <router-link to="/controles" class="create-account nav-link" >Controles</router-link>
+        <router-link 
+          to="/controles" 
+          class="create-account nav-link" 
+          :class="{ 'selected': selectedButton === '/controles' }"
+          @click="selectedButton = '/controles'"
+        >
+          Controles
+        </router-link>
       </div>
       <div class="main-nav__item">
-        <router-link to="/accion" class="create-account nav-link">Acción</router-link>
+        <router-link 
+          to="/accion" 
+          class="create-account nav-link" 
+          :class="{ 'selected': selectedButton === '/accion' }"
+          @click="selectedButton = '/accion'"
+        >
+          Acción
+        </router-link>
       </div>
       <div class="main-nav__item">
-        <router-link to="/seguimiento" class="create-account nav-link">Seguimiento</router-link>
+        <router-link 
+          to="/seguimiento" 
+          class="create-account nav-link" 
+          :class="{ 'selected': selectedButton === '/seguimiento' }"
+          @click="selectedButton = '/seguimiento'"
+        >
+          Seguimiento
+        </router-link>
       </div>
     </ul>
   </nav>
-  </template>
-  
-<script setup>
+</template>
 
+<script setup>
+import { ref } from 'vue';
+
+// Variable reactiva para almacenar el botón seleccionado
+const selectedButton = ref('');
 </script>
 
-
 <style scoped>
-
 .navigation {
-grid-column: 5 / 17;
-grid-row: 2 / 3;
-margin-top: 50px;
+  grid-column: 5 / 17;
+  grid-row: 2 / 3;
+  margin-top: 50px;
 }
 
 .main-nav {
-display: flex;
-justify-content: space-around;
+  display: flex;
+  justify-content: space-around;
 }
 
-
 .nav-link {
-padding: 10px 40px;
-color: white;
-border-radius: 10px;
-text-decoration: none;
-background-color: #c91717;
+  padding: 10px 40px;
+  color: white;
+  border-radius: 10px;
+  text-decoration: none;
+  background-color: #c91717;
+  transition: all 0.3s ease;
 }
 
 .nav-link:hover {
-background-color: white;
-color: red;
+  background-color: white;
+  color: red;
 }
-  
-  
-  
-</style> 
+
+/* Estilo para el botón seleccionado */
+.nav-link.selected {
+  background-color: white;
+  color: red;
+  border: 2px solid #c91717; /* Opcional: para destacar */
+}
+</style>
