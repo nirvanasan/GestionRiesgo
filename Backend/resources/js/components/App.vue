ebf4c8fec7347ad9886f1599e9c928d535313608
DiegoCopete <diego.lloreda@tecnoevolution.com.co>
2025-01-08 11:18:07 -0500

    se arregla schroll de la pagina ´principal

--- Backend/resources/js/components/App.vue
+++ Backend/resources/js/components/App.vue
@@ -29,3 +29,14 @@ export default {
   }
 };
 </script>
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%; /* Asegura que el body ocupe toda la altura */
+  overflow: hidden; /* Evita scroll innecesario */
+  
+}
+
+
+</style>
